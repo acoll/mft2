0fd9f39d281b11b6f462ce440543403907045eff
Adam <(none)>
2018-01-21 17:30:59 -0500

    update manifest colors

--- src/components/button.js
+++ src/components/button.js
@@ -1,51 +1,5 @@
 import { h } from "preact";
 import styled from "styled-components";
-// import theme from "../style/theme.js";
-
-// const { sidePadding, lightFontColor, darkBg } = theme;
-
-// button {
-// 	position: relative;
-// 	font-size: 16px;
-// 	padding: 16px 0px;
-// 	background-color: rgba(236, 78, 28, 0.9);
-// 	border: none;
-// 	border-radius: 4px;
-// 	box-shadow: inset -1px -1px 1px rgba(0, 0, 0, 0.25);
-// 	display: grid;
-// 	grid-template-columns: 1fr 1fr 1fr;
-
-// 	.label {
-// 	  /* grid-column: 2/3; */
-// 	}
-
-// 	.spinner {
-// 	  /* grid-column: 3/4; */
-// 	  transition: opacity 250ms ease-in;
-// 	  &.active {
-// 		opacity: 0;
-// 	  }
-
-// 	  &:after {
-// 		content: " ";
-// 		width: 1.5rem;
-// 		height: 1.5rem;
-// 		border-top-color: #444;
-// 		border-left-color: #444;
-
-// 		/* Additional spinner styles */
-// 		animation: spinner 400ms linear infinite;
-// 		border-bottom-color: transparent;
-// 		border-right-color: transparent;
-// 		border-style: solid;
-// 		border-width: 2px;
-// 		border-radius: 50%;
-// 		box-sizing: border-box;
-// 		display: inline-block;
-// 		vertical-align: middle;
-// 	  }
-// 	}
-//   }
 
 const Button = styled.button`
   font-size: 16px;
